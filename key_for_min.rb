fce47cc6b1fb7c790b102ff0e1be1aa850bcdc66
Luis Alberto Servin <lservin2015@gmail.com>
2020-02-03 21:28:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil 
   name_hash.each do |key, value|
     if lowest_value == 0 || value < lowest_value
-      lowest value = value
+      lowest_value = value
       lowest_key = key
     end
   end
